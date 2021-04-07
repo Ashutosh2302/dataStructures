b26474131d458676a88f7ae15737c813685df7b1
Ashutosh Bansal <56742106+Ashutosh2302@users.noreply.github.com>
2021-04-07 10:56:10 +0100

    starting data structures

---
+++ main.py
@@ -0,0 +1,9 @@
+def menu():
+    print("1. Arrays")
+    print("2. Strings")
+    print("3. LinkedLists")
+    print("4. Tree")
+    print("5. Graphs")
+    return int(input("Enter your choice"))
+
+print(menu())
